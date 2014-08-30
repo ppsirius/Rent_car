e2938867fcc5176fc1078c1c2d3caf3a04cac386
Kuba Wróbel <sklerenchyma@gmail.com>
2014-08-30 16:45:47 +0200

    Brand model tests finished\!

--- test/models/brand_test.rb
+++ test/models/brand_test.rb
@@ -1,6 +1,20 @@
 require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
+
+  def brand_params
+    {name: "Mercedes", origin: "Germany"}
+  end
+
+  test "Cannot have a car brand name without that name" do
+    brand = Brand.new(brand_params.merge(name: nil))
+    assert_not brand.save, "Created a brand name without a name (a paradox ?)"
+  end
+
+  test "Cannot have a car brand name without knowing a car brand country of origin" do
+    brand = Brand.new(brand_params.merge(origin: nil))
+    assert_not brand.save, "Created a brand name without knowing hers country of origin"
+  end
   # test "the truth" do
   #   assert true
   # end
