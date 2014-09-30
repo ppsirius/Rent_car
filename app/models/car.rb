c861d17ca91c64f068afb5f0d41186eb4bf1991b
Piotr Pupczyk <piotrek@jakbyco.com>
2014-10-01 00:27:09 +0200

    add test scope and change car index

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,35 +3,33 @@ class Car < ActiveRecord::Base
   belongs_to :type
   has_many :clients, through: :rents
 
-
   validates :state, :car_paint, :plate, :production_year, :type_id, presence: true
   validates :plate, uniqueness: true
 
+  scope :available, -> { where(rents_count: 0) }
 
 	state_machine :state, initial: :available do
 
-	event :rent do
-	 transition :available => :rented
-	end
+  	event :rent do
+  	 transition :available => :rented
+  	end
 
-  event :return do
-   transition :rented => :available
+    event :return do
+     transition :rented => :available
+    end
+
+    event :clean do
+     transition :available => :clean, :serviced => :clean
+    end
+
+  	event :service do
+  	 transition :available => :serviced, :clean => :serviced
+  	end
+
+    event :available do
+      transition :clean => :available, :serviced => :available, :rented => :available
+    end
   end
-
-  event :clean do
-   transition :available => :clean, :serviced => :clean
-  end
-
-	event :service do
-	 transition :available => :serviced, :clean => :serviced
-	end
-
-  event :available do
-    transition :clean => :available, :serviced => :available, :rented => :available
-  end
-
-end
-
 
   def to_label
     "#{type.brand.name} #{type.name}"
