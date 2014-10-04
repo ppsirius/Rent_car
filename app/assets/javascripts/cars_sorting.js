5d1276c2c5fd1b6d7b73f2e307478b34ea0e236c
Jakub Nocula <jakub.nocula@gmail.com>
2014-10-04 15:25:42 +0200

    to fix

--- app/assets/javascripts/cars_sorting.js
+++ app/assets/javascripts/cars_sorting.js
@@ -1,5 +1,29 @@
 $(function(){
-  $('#car_production_year').keyup(function (e){
+
+  var clickHandler = function (e) {
+    console.log(this);
+    $.ajax({
+      url: "cars",
+      data: {sort_by: $(this).attr('id').split('-')[0]},
+      dataType: "script"
+    });
+    console.log($(this).attr('id').split('-')[0])
+
+    return false;
+  };
+
+
+  $('#type-sort').click(clickHandler);
+
+  var links = ['type-sort', 'brand-sort', 'plate-sort', 'color-sort', 'productionYear-sort', 'state-sort'];
+
+  for(var i = 0; i < links.length; i++) {
+    $('#'+links[i]).click(clickHandler);
+  }
+
+});
     
+
+
 
 
