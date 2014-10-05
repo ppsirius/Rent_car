3d96bf543ac3aa03395e660bce515e9b29315b6f
Piotr Pupczyk <piotrek@jakbyco.com>
2014-10-05 12:27:30 +0200

    rents index

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,4 +5,11 @@ class Client < ActiveRecord::Base
 	validates :name, :surname, :age, :doc_type, :doc_number, presence: true
 	validates :age, numericality: true, inclusion: {in: 18..80}
 	validates :doc_number, uniqueness: true
+
+  
+
+  def full_name
+    name + ' ' + surname 
+  end
+
 end
--- app/models/rent.rb
+++ app/models/rent.rb
@@ -3,4 +3,20 @@ class Rent < ActiveRecord::Base
   belongs_to :car
   belongs_to :client
 
+  delegate :full_name, to: :client, allow_nil: true
+  delegate :type_name, to: :car, allow_nil: true
+  delegate :type_brand_name, to: :car, allow_nil: true
+
+  def display_date
+    created_at.strftime("%d/%m/%Y at %R")
+  end
+
+  def rented
+    if active == true
+      "Rented"
+    else
+      "Returned"
+    end 
+
+  end
 end
